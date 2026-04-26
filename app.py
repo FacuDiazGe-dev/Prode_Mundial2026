4db970be4d128355271c62997d1f4874a0a40aa1
FacuDiazGe-dev <facundodiazgramajo@gmail.com>
2026-04-25 23:04:50 -0300

    Update app.py

--- app.py
+++ app.py
@@ -316,12 +316,12 @@ if menu == "🏠 Inicio":
 elif menu == "📝 Mis Pronósticos":
     with col_principal:
         st.subheader("📝 Mis Predicciones")
-        ahora_arg = datetime.now() - timedelta(hours=-3)        
+        ahora_arg = datetime.now() - timedelta(hours=3)        
         
         # --- CONFIGURACIÓN DE FECHA LÍMITE ---
         # Fecha límite: 8 de junio de 2026 a las 23:59:59
         
-        fecha_limite = datetime(2026, 4, 25, 23, 00, 00)
+        fecha_limite = datetime(2026, 6, 8, 23, 59, 59)
         ahora = datetime.now()
         es_tiempo_valido = ahora < fecha_limite
         
