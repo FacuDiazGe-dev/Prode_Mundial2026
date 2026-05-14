c2861412466a788e64ba56a30c7cc71a24242f69
FacuDiazGe-dev <facundodiazgramajo@gmail.com>
2026-05-13 23:10:24 -0300

    Update styles_config.py

--- styles_config.py
+++ styles_config.py
@@ -70,8 +70,8 @@ html, body, [class*="css"] {
     background-color: #07111F;
     background-image:
         linear-gradient(
-            rgba(7,17,31,0.67),
-            rgba(7,17,31,0.76)
+            rgba(7,17,31,0.57),
+            rgba(7,17,31,0.66)
         ),
         url("__SIDEBAR_BANNER__");
     background-size: cover;
