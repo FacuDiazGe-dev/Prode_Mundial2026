469af40234982237a3adc4131924b601c2f76505
FacuDiazGe-dev <facundodiazgramajo@gmail.com>
2026-05-17 01:17:59 -0300

    Update jugadores.py

--- sections/jugadores.py
+++ sections/jugadores.py
@@ -1544,7 +1544,7 @@ loading="lazy"
 </div>
 """, unsafe_allow_html=True)
 
-            with st.container(height=300):
+            with st.container(height=310):
                 for _, u in df_usuarios.iterrows():
                     foto_mini = get_avatar(u)
 
