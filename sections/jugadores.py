399fad4d264beb814f59373b02c48ac5ba1b33ff
FacuDiazGe-dev <facundodiazgramajo@gmail.com>
2026-05-17 00:15:44 -0300

    Update jugadores.py

--- sections/jugadores.py
+++ sections/jugadores.py
@@ -264,8 +264,8 @@ def render_jugadores(
     background:
         linear-gradient(
             90deg,
-            rgba(255,255,255,0.92),
-            rgba(255,255,255,0.76)
+            rgba(255,255,255,0.35),
+            rgba(255,255,255,0.15)
         ),
         var(--player-hero-bg);
 
