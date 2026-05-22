ca579a7734f12e4280194c7dd64e1ef7a6b0c613
FacuDiazGe-dev <facundodiazgramajo@gmail.com>
2026-05-22 15:23:51 -0300

    Update supabase_service.py

--- services/supabase_service.py
+++ services/supabase_service.py
@@ -1,1 +1,62 @@
+import streamlit as st
+import pandas as pd
+from supabase import create_client
 
+
+@st.cache_resource
+def get_supabase_client():
+    url = st.secrets["SUPABASE_URL"]
+    key = st.secrets["SUPABASE_KEY"]
+
+    return create_client(url, key)
+
+
+@st.cache_data(ttl=300)
+def get_resultados_supabase():
+    supabase = get_supabase_client()
+
+    response = (
+        supabase
+        .table("resultados")
+        .select("*")
+        .order("n_partido")
+        .execute()
+    )
+
+    data = response.data or []
+
+    return pd.DataFrame(data)
+
+
+@st.cache_data(ttl=300)
+def get_usuarios_supabase():
+    supabase = get_supabase_client()
+
+    response = (
+        supabase
+        .table("usuarios")
+        .select("*")
+        .order("usuario")
+        .execute()
+    )
+
+    data = response.data or []
+
+    return pd.DataFrame(data)
+
+
+@st.cache_data(ttl=300)
+def get_pronosticos_supabase():
+    supabase = get_supabase_client()
+
+    response = (
+        supabase
+        .table("pronosticos")
+        .select("*")
+        .order("usuario")
+        .execute()
+    )
+
+    data = response.data or []
+
+    return pd.DataFrame(data)
