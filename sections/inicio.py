4401a90970b7e49856db07de85f6efca96f973b1
FacuDiazGe-dev <facundodiazgramajo@gmail.com>
2026-05-24 02:48:32 -0300

    Update inicio.py

--- sections/inicio.py
+++ sections/inicio.py
@@ -1178,7 +1178,7 @@ def render_inicio(
     """, unsafe_allow_html=True)
 
     FONDO_CARD_INICIO = "https://storage.googleapis.com/foto-prode2026/Banners/FONDO_CARD_INICIO.png"
-    FONDO_CARD_GOLD = "https://storage.googleapis.com/foto-prode2026/Banners/FONDO_CARD_GOLD.png"
+    FONDO_CARD_GOLD = "https://storage.googleapis.com/foto-prode2026/Banners/FONDO_CARD_GOLD2.png"
     FONDO_CARD_SILVER = "https://storage.googleapis.com/foto-prode2026/Banners/FONDO_CARD_SILVER.png"
     FONDO_CARD_BRONCE = "https://storage.googleapis.com/foto-prode2026/Banners/FONDO_CARD_BRONCE.png"
 
